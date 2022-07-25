8416fcc3300ffd3f511ce9c5ba945117554100b4
Vinod Jagtap <vinod.jagtap@hotmail.com>
2022-07-25 21:15:48 +0530

    Initial Commit

---
+++ QRScanSwiftUI/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  QRScanSwiftUI
+//
+//  Created by Vinod Jagtap on 25/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QRScanSwiftUI/QRScanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScanSwiftUIApp.swift
+//  QRScanSwiftUI
+//
+//  Created by Vinod Jagtap on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct QRScanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
